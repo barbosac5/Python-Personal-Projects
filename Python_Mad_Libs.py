fbea025aa437e74029ca02960ba85860bf1b6832
Christian Barbosa <102810988+barbosac5@users.noreply.github.com>
2024-05-01 11:25:37 -0400

    Add files via upload
    Added second personal project: Python Mad Libs

---
+++ Python_Mad_Libs.py
@@ -0,0 +1,22 @@
+## Guve the user the ability to input their selected nouns, verbs, ajd,. etc
+noun = input("Place a noun here: ")
+noun_2 =  input("Place a second noun here: ")
+noun_3 = input("Place a final noun here")
+pronoun = input("Place a pronoun here: ")
+pronoun_2 = input("Place another pronoun: ")
+adj = input("Place an adjective here: ")
+adj_2 = input("Place a second adjective here: ")
+adj_3 = input("Place a final adj here: ")
+verb = input("Place a verb here: ")
+verb_2 = input("Place a second verb here: ")
+verb_3 = input("Place the final verb here: ")
+name = input("Which name would you like to use? ")
+
+
+## Create a sentence using the variables above
+##sentence = "One morning, the " + adj + " " + noun + " decided to " + verb + " so then they would feel less bored with each passing " + noun_2 + "."
+##print(sentence)
+
+## Create a story
+story = name + " one day decided to " + verb + " on his skills in " + verb_2 + " in order to " + verb_3 + " grander events in his " + adj + " life. " + pronoun + " wanted to inprove on the " + adj + " skillset through " + verb + " so then " + pronoun_2 + " would feel less " + adj_2 + "since the job market has always been " + adj_3 + ". " + pronoun + " " + verb_3 + " to do this in a " + noun + " that looked like a " + noun_2 + " which looked a little strange to " + pronoun_2 + "." + pronoun + " then started to buckle " + pronoun_2 + " " + noun_3 + "and " + verb_3 + " everyday to eventually " + verb_2 + " and feel less " + adj + ". " + "Over time" + pronoun_2 + " got more comfotable " + verb + " and started to " + verb_2 + " it and started to show the " + noun_2 + " to employers."
+print(story)
