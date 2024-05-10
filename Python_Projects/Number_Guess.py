02395b6cd533894e3f99fefe20069dcd0e9e8920
Christian Barbosa <102810988+barbosac5@users.noreply.github.com>
2024-05-10 11:53:48 -0400

    Add files via upload

--- Python_Projects/Number_Guess.py
+++ Python_Projects/Number_Guess.py
@@ -3,27 +3,29 @@ import math
 import random
 
 # Function for number guess
-def number_guess(number):
-    print("Welcome to the Number Guessing Game. You will have 5 tries to guess my number. Good luck!")
-    # Have program generate a random number for the player to guess
-    n  = random.randint(1, 10)
+def number_guess(n):
     # Have starting guess count set to 0
     guess_count = 0
-    # Prompt to allow user to input their first, second, third, etc. guess
-    guess = int(input("What is your guess?: "))
-    if guess > n:
-        print("Too high. Try again.")
-        guess_count += 1
-    elif guess < n:
-        print("Too low. Try again")
-        guess_count += 1
-    else:
-        print("You got it! Good job! To took you " + str(guess_count) + " attempts!") 
-       
+    print("Welcome to the Number Guessing Game. You will have 3 tries to guess my number. Good luck!")
+    while guess_count < 3:
+        # Have program generate a random number for the player to guess
+        n  = random.randint(1, 10)
+        # Prompt to allow user to input their first, second, third, etc. guess
+        guess = int(input("What is your guess?: "))
+        if guess == n:
+            print("You got it! Good job! To took you " + str(guess_count) + " attempts!")
+            break
+        else:
+            if guess_count == 2:
+                print("Game Over")
+                break
+            elif guess > n:
+                print("Too high. Try again.")
+                guess_count += 1
+            elif guess < n:
+                print("Too low. Try again")
+                guess_count += 1
         
-
-
-
-
+       
 # Call the function
-number_guess(45)
+number_guess(9)
