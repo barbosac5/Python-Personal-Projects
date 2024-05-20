158bbb2f21c6ad152d12eca1204e001fc3b25c86
Christian Barbosa <102810988+barbosac5@users.noreply.github.com>
2024-05-20 11:10:20 -0400

    Add files via upload
    Updated project some tweaks made

--- Python_Projects/Rock_Paper_Scissors.py
+++ Python_Projects/Rock_Paper_Scissors.py
@@ -3,23 +3,36 @@ import random
 
 # Choices 
 choices = ["Rock", "Paper", "Scissors"]
+ 
 
-# User input
-guess = input("Rock, Paper, Scissors.... SHOOT! ")
-if guess != "Rock" "Paper" or "Scissors":
-    print("Not a choice")
-    quit()      
+# Game count
+game_count  = 0
 
+# Loop to count best of 3 games
+while game_count < 3:     
+    ## User input
+    guess = input("Rock, Paper, Scissors.... SHOOT! ")
+    #guess = guess.lower()
 
-# Computer make a random choice
-CPU_choice = random.choice(choices)
-print("You guessed " + guess + " while the CPU guessed.. " + CPU_choice)
+    ## If user makes invalid guess
+    if guess != "Rock" "Paper" or "Scissors":
+        print("Not a choice")
+        quit()
 
-# Determine win, loss or tie
-if guess == "Rock" and CPU_choice == "Rock":
-    print("It's a tie!")
-elif guess == "Rock" and CPU_choice == "Paper":
-    print("You Lose")
-elif guess == "Rock" and CPU_choice == "Scissors":
-    print("You Win!")
+    ## Computer make a random choice
+    CPU_choice = random.choice(choices)
+    print("You guessed " + guess + " while the CPU guessed.. " + CPU_choice)
+    game_count += 1
+
+    ## Determine win, loss or tie
+    if guess == "Rock" and CPU_choice == "Rock":
+        print("It's a tie!")
+    elif guess == "Rock" and CPU_choice == "Paper":
+        print("You Lose")
+    elif guess == "Rock" and CPU_choice == "Scissors":
+        print("You Win!")
+    
+
+    
+    
 
