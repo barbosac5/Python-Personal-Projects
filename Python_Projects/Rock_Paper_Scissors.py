d9e95f7cf841d7b25cfe243b389639e041351b53
Christian Barbosa <102810988+barbosac5@users.noreply.github.com>
2024-05-24 11:44:20 -0400

    Add files via upload
    Updated Code

--- Python_Projects/Rock_Paper_Scissors.py
+++ Python_Projects/Rock_Paper_Scissors.py
@@ -4,33 +4,64 @@ import random
 # Choices 
 choices = ["Rock", "Paper", "Scissors"]
  
+# Set intial game count
+game_count = 0
 
-# Game count
-game_count  = 0
-
-# Loop to count best of 3 games
-while game_count < 3:     
-    ## User input
-    guess = input("Rock, Paper, Scissors.... SHOOT! ")
+## User input
+prompt = print("Rock, Paper, Scissors, SHOOT:")
+guess = input("")
     #guess = guess.lower()
 
-    ## If user makes invalid guess
-    if guess != "Rock" "Paper" or "Scissors":
-        print("Not a choice")
-        quit()
-
+# Loop to count best of 3 games
+while game_count < 2:     
+        
     ## Computer make a random choice
     CPU_choice = random.choice(choices)
     print("You guessed " + guess + " while the CPU guessed.. " + CPU_choice)
     game_count += 1
-
-    ## Determine win, loss or tie
+    
+    ## Determine win, loss or tie (for Rock)
     if guess == "Rock" and CPU_choice == "Rock":
         print("It's a tie!")
+        print(input("Lets go Again: "))
     elif guess == "Rock" and CPU_choice == "Paper":
         print("You Lose")
+        print(input("Lets go Again: "))
     elif guess == "Rock" and CPU_choice == "Scissors":
         print("You Win!")
+        print(input("Lets go Again: "))
+    
+    ## Determine win, loss or tie (for Paper)
+    elif guess == "Paper" and CPU_choice == "Rock":
+        print("You Win!")
+        print(input("Lets go Again: "))
+    elif guess == "Paper" and CPU_choice == "Paper":
+        print("It's a tie")
+        print(input("Lets go Again: "))
+    elif guess == "Paper" and CPU_choice == "Scissors":
+        print("You Lose!")
+        print(input("Lets go Again: "))
+
+    ## Determine win, loss or tie (for Scissors)
+    elif guess == "Scissors" and CPU_choice == "Rock":
+        print("You Lose!")
+        print(input("Lets go Again: "))
+    elif guess == "Scissors" and CPU_choice == "Paper":
+        print("You Win")
+        print(input("Lets go Again: "))
+    elif guess == "Scissors" and CPU_choice == "Scissors":
+        print("It's a tie")
+        print(input("Lets go Again: "))
+    else:
+        print("Not a valid choice")
+        quit()
+
+if game_count == 3:
+    print("Game Over")
+
+
+
+    
     
 
     
